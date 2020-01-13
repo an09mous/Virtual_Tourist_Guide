1d4f59d583868899f36867540465ee437abf440b
the_adeshkhanna <adeshkhanna271@gmail.com>
2020-01-13 20:41:33 +0530

    [setup] project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(GoGoa());
+
+class GoGoa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GoGoa',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
